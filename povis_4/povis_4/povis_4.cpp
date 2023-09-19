a5f899b298649b461991a69ac15cceb2ad3f0aa5
vladislav245 <69549451+vladislav245@users.noreply.github.com>
2023-09-19 18:59:58 +0300

    Add files via upload

---
+++ povis_4/povis_4/povis_4.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include "Book.h"
+
+
+int main()
+{
+
+	setlocale(LC_ALL, "RUS");
+
+
+	Scripture obj1("Christianity");
+
+	obj1.Output();
+	getchar();
+
+	obj1.Input();
+
+	std::cout << std::endl << std::endl << "Результат" << std::endl;
+	obj1.Output();
+	getchar();
+
+	Magazine obj2(3, "Vlad", "policy", "New York Nimes", 0);
+
+	obj2.Output();
+	getchar();
+
+	obj2.Input();
+
+	std::cout << std::endl << std::endl << "Результат" << std::endl;
+	obj2.Output();
+	getchar();
+
+	AD_Magazine obj3("clothes", 6, "Bob");
+
+	obj3.Output();
+	getchar();
+
+	obj3.Input();
+
+	std::cout << std::endl << std::endl << "Результат" << std::endl;
+	obj3.Output();
+	getchar();
+
+	return 0;
+}
+
